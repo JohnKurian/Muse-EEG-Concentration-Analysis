6fab50efc46d46409baa6a0a6881894db9be3ad7
avinashronanki <ronanki.avinash@gmail.com>
2021-10-01 14:34:35 +0200

    added the username, activity and file name
    Edit the file name and the scores of the game manually, please follow the naming convention as mentioned in the high_frequency_datasets folder

--- bluetooth_recorder.py
+++ bluetooth_recorder.py
@@ -1,3 +1,9 @@
 from muselsl import record
+import uuid
+username = "avi"
+activity = "lostinmigration"
 
-record(60, data_source="EEG", dejitter=False)
+filename = "./" + username + "_" + activity + "_ " + str(uuid.uuid4()) + ".csv"
+
+
+record(47, data_source="EEG", dejitter=False, filename= filename)
